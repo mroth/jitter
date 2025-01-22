0fb2b418049e4aee87ceb4850bc0917be7d5f2e8
Matthew Rothenberg <mrothenberg@gmail.com>
2025-01-22 15:58:13 -0500

    docs: remove an old reference to seeding rand in examples

--- example_test.go
+++ example_test.go
@@ -2,17 +2,12 @@ package jitter_test
 
 import (
 	"fmt"
-	"math/rand"
 	"time"
 
 	"github.com/mroth/jitter"
 )
 
 func ExampleNewTicker() {
-	// jitter uses the global random source, seed it appropriately. for this
-	// example, we seed it to a constant for predictable output.
-	rand.Seed(42)
-
 	// ticker with base duration of 10 milliseconds and 0.5 scaling factor
 	ticker := jitter.NewTicker(10*time.Millisecond, 0.5)
 	defer ticker.Stop()
@@ -23,17 +18,4 @@ func ExampleNewTicker() {
 		fmt.Println("Time elapsed since last tick: ", t.Sub(prev))
 		prev = t
 	}
-}
-
-func ExampleScale() {
-	rand.Seed(1)
-	for i := 0; i < 5; i++ {
-		fmt.Println(jitter.Scale(time.Second, 0.5))
-	}
-	//Output:
-	// 1.44777941s
-	// 582.153551ms
-	// 1.166145821s
-	// 735.010051ms
-	// 787.113937ms
 }
