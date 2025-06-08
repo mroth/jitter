19b913e3b6b6bcb4a019544b9471ca96575c3908
Matthew Rothenberg <mrothenberg@gmail.com>
2025-06-08 10:59:23 -0400

    refactor: utilize rand/v2 on >=go1.22

---
+++ rand_go1.22.go
@@ -0,0 +1,17 @@
+//go:build go1.22
+// +build go1.22
+
+package jitter
+
+import "math/rand/v2"
+
+// randRange returns a nonnegative pseudo-random number in the half open
+// interval [min, max) from the default Source.
+//
+// It panics if max < min.
+func randRange(min, max int64) int64 {
+	if min == max {
+		return min
+	}
+	return rand.Int64N(max-min) + min
+}
