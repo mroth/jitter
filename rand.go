917edf61a11ca030f8e67e3b8e8dcb00ddca6761
Matthew Rothenberg <mroth@mroth.xyz>
2025-08-14 09:10:33 -0400

    cleanup: remove build constraints around math/rand/v2 use
    no longer required as minimum supported version is above go1.22

--- rand.go
+++ rand.go
@@ -1,9 +1,6 @@
-//go:build !go1.22
-// +build !go1.22
-
 package jitter
 
-import "math/rand"
+import "math/rand/v2"
 
 // randRange returns a nonnegative pseudo-random number in the half open
 // interval [min, max) from the default Source.
@@ -13,5 +10,5 @@ func randRange(min, max int64) int64 {
 	if min == max {
 		return min
 	}
-	return rand.Int63n(max-min) + min
+	return rand.Int64N(max-min) + min
 }
